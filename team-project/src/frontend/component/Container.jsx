8789fac22c2eed8dcbeb93792110c59f9afcb11e
hiheeen <gmldms0338@naver.com>
2023-07-14 13:25:45 +0900

    질문페이지 구현

--- team-project/src/frontend/component/Container.jsx
+++ team-project/src/frontend/component/Container.jsx
@@ -42,19 +42,30 @@ to {
     opacity:0;
 }
 `;
+const fadeInAnimation = keyframes`
+from {
+    opacity : 0;
+}
+to {
+    opacity : 1;
+}
+`;
 const Grid = styled.div`
     display: flex;
     flex-direction: column;
     width: 500px;
 
     &.click {
-        animation: ${fadeOutAnimation} 0.5s ease-out;
+        animation: ${fadeOutAnimation} 0.6s ease-out;
+    }
+    &.next {
+        animation: ${fadeInAnimation} 0.6s ease-in;
     }
 `;
-let count = 0;
 
 function Container(props) {
     const questionBox = [
+        '오늘은 오랜만에 쉬는 날! 머리가 조금 복잡해져서 밖으로 나가려고 하는데, 평일에 어떤 고민을 해왔을까?',
         '오늘은 힐링을 위해 소품샵에 가려고 외출을 하였다. 소품샵을 가는 길에 언젠가 한번 가보려고 했던 가게가 보인다. 이 때, 당신에게 드는 생각은?',
         '소품샵에 들어가자마자 마음에 쏙 드는 물건을 발견한 당신! 당신은 어떤 것을 더 먼저 보는가?',
         '소품샵을 둘러보던 도중, 벽에 액자가 걸려있는 것을 발견한다. 그런데..! 여기 분위기랑은 조금 맞지 않은 것 같다. 이 때 떠오른 생각은?',
@@ -62,6 +73,10 @@ function Container(props) {
         '사람이 더 많아진 것 같다…! 주위에서 웅성대는 소리가 들려 고개를 돌려보았더니 직원이 진상손님이랑 말다툼을 벌이고 있다. 이 때, 당신이 드는 생각은?',
         '진상손님이 나간 이후 조금 잠잠해진 소품샵에서 소품샵을 본격적으로 쇼핑을 하려고 하는데, 어떻게 구경할 것인가?',
         '가게를 다 둘러보고 이제 가려고 한다.  오늘의 쇼핑이 꽤나 마음에 들었던 당신! 당신의 행동은?',
+        '9',
+        '10',
+        '11',
+        '12',
     ];
     const answerBoxA = [
         { mbti: 'N', answer: '미래에 대한 두루뭉실한 고민' },
@@ -78,6 +93,10 @@ function Container(props) {
         { mbti: 'F', answer: '아.. 저 직원 진짜 힘들겠다; ' },
         { mbti: 'P', answer: '발길이 닿는대로 움직인다.' },
         { mbti: 'E', answer: '여긴 나만 알기엔 아까워! 동네방네 소문낸다.' },
+        { mbti: '1a', answer: '1a' },
+        { mbti: '2a', answer: '2a' },
+        { mbti: '3a', answer: '3a' },
+        { mbti: '4a', answer: '4a' },
     ];
     const answerBoxB = [
         { mbti: 'S', answer: '오늘 당장 해야하는 것에 대한 고민' },
@@ -91,68 +110,76 @@ function Container(props) {
         { mbti: 'T', answer: '엇 저 사람들 왜 싸우지??' },
         { mbti: 'J', answer: '1층부터 정해진 방향대로 움직인다. ' },
         { mbti: 'I', answer: '여긴 나만 알고 싶어! 다이어리에 끄적여놓는다.' },
+        { mbti: '1b', answer: '1b' },
+        { mbti: '2b', answer: '2b' },
+        { mbti: '3b', answer: '3b' },
+        { mbti: '4b', answer: '4b' },
     ];
-    const [question, setQuestion] = useState(
-        '오늘은 오랜만에 쉬는 날! 머리가 조금 복잡해져서 밖으로 나가려고 하는데, 평일에 어떤 고민을 해왔을까?'
-    );
+    const [question, setQuestion] = useState('');
     const [answerA, setAnswerA] = useState('');
     const [answerB, setAnswerB] = useState('');
     const [array, setArray] = useState([]);
+    const [count, setCount] = useState(0);
 
     const [color, setColor] = useState('none');
     const [isClick, setIsClick] = useState(false);
     useEffect(() => {
         setAnswerA(answerBoxA[count].answer);
         setAnswerB(answerBoxB[count].answer);
+        setQuestion(questionBox[count]);
     }, []);
 
     const handleAnswerA = () => {
-        if (count < 8) setIsClick(true);
+        if (count < 11) setIsClick(true);
         setTimeout(() => {
             setArray((prevArray) => [
-                { [answerBoxA[count - 1].mbti]: answerA },
+                { [answerBoxA[count].mbti]: answerA },
                 ...prevArray,
             ]);
-            if (count === 8) {
-                // setAnswerA(answerBoxA[6].answer);
-                // setAnswerB(answerBoxB[6].answer);
-                // setQuestion(questionBox[6]);
-                count++;
+            console.log(count - 1);
+            if (count === 11) {
+                setAnswerA(answerBoxA[11].answer);
+                setAnswerB(answerBoxB[11].answer);
+                setQuestion(questionBox[11]);
+                setCount(count + 1);
                 return;
             } else {
                 setAnswerA(answerBoxA[count + 1].answer);
                 setAnswerB(answerBoxB[count + 1].answer);
-                setQuestion(questionBox[count]);
+                setQuestion(questionBox[count + 1]);
                 setIsClick(false);
+                // count++; //얘랑
+                setCount(count + 1);
+                console.log(array); //얘를 먼저 하는듯. set 함수는 나중에
             }
 
-            count++;
-            console.log(array);
+            // count++;
+            // console.log(array);
         }, 500);
     };
     const handleAnswerB = () => {
-        if (count < 8) setIsClick(true);
+        if (count < 11) setIsClick(true);
         setTimeout(() => {
             setArray((prevArray) => [
-                { [answerBoxB[count - 1].mbti]: answerB },
+                { [answerBoxB[count].mbti]: answerB },
                 ...prevArray,
             ]); // setState함수인 setArray는 비동기적으로 작동하기 때문에
             // 상태 업데이트를 한 직후에 상태를 확인할 수 없다. 그래서 다음 버튼 눌렀을 때에야 업데이트 된 상태를 확인가능
             // 값이 들어오긴 함 -> 마지막 8번 누르고 count가 8이되니까 조건문으로 결과보기 버튼 구현할 것.
             // 8번 답 누르면 결과보기 버튼 나오고(8번까지 저장된 상태) , 결과보기 버튼 누르면 그대로 전송 or 결과 계산
-            if (count === 8) {
-                // setAnswerA(answerBoxA[6].answer);
-                // setAnswerB(answerBoxB[6].answer);
-                // setQuestion(questionBox[6]);
-                count++;
+            if (count === 11) {
+                setAnswerA(answerBoxA[11].answer);
+                setAnswerB(answerBoxB[11].answer);
+                setQuestion(questionBox[11]);
+                setCount(count + 1);
             } else {
                 setAnswerA(answerBoxA[count + 1].answer);
                 setAnswerB(answerBoxB[count + 1].answer);
-                setQuestion(questionBox[count]);
+                setQuestion(questionBox[count + 1]);
                 setIsClick(false);
+                setCount(count + 1);
             }
 
-            count++;
             console.log(array);
         }, 500);
     };
@@ -164,7 +191,7 @@ function Container(props) {
                     <QBox className="box_question">
                         <div>{question}</div>
                     </QBox>
-                    <Grid className={`grid ${isClick ? 'click' : ''}`}>
+                    <Grid className={`grid ${isClick ? 'click' : 'next'}`}>
                         <Box
                             className="box_answer_1"
                             handleClick={handleAnswerA}
@@ -176,7 +203,7 @@ function Container(props) {
                             text={answerB}
                         />
                     </Grid>
-                    {count === 9 ? (
+                    {count === 12 ? (
                         <div
                             style={{
                                 width: 500,
