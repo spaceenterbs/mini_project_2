62e01688e460f23b6d4619b0e2abda269a42cbfe
dozzzini <dozzzini@naver.com>
2023-07-14 22:56:20 +0900

    카카오톡 api 구현

--- team-project/src/App.js
+++ team-project/src/App.js
@@ -1,29 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 import GlobalStyles from "./GlobalStyles"; //전역에 css 적용하는 js 컴포넌트
 import { useState, useEffect } from "react";
 import Container from "./frontend/component/Container";
 import Start from "./frontend/startpage/Start";
 import Result from "./frontend/resultpage/Result";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Progress from './frontend/component/Progress';
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Progress from "./frontend/component/Progress";
 
 function App(props) {
   return (
-      <div>
-          <GlobalStyles />
-          <Progress />
-          {/* <BrowserRouter>
-              <Routes>
-                  <Route index element={<Start />} />
-                  <Route path="Container" element={<Container />} />
-                  <Route path="Result" element={<Result />} />
-              </Routes>
-          </BrowserRouter> */}
-      </div>
+    <div>
+      <GlobalStyles />
+      {/* <Progress /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Start />} />
+          <Route path="Container" element={<Container />} />
+          <Route path="Result" element={<Result />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
+}
 
-
+export default App;
